34e7e1c81f5e813d2d5ba6391dfbe042a18dee8a
Thomas Pope <tpope0928@gmail.com>
2019-01-05 22:06:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,9 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.drop_table
-    
+ def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
   end
 
 
