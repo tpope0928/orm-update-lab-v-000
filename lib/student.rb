57f44fe02e8bd3a16135739eba0f5964281397c4
Thomas Pope <tpope0928@gmail.com>
2019-01-05 22:02:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,15 @@ class Student
   end
   
   def self.create_table
-    
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
   end
 
 
