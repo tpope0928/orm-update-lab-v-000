f2834784b0bb6c2f929105c7d9f523921b89dd66
Thomas Pope <tpope0928@gmail.com>
2019-01-05 22:19:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,10 @@ class Student
   end
   
   def self.find_by_name(name)
-    
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
     
     
   
